1aadaf92146c68bea860a17f17ac1bbbcd42da82
samuellna <sna2@cin.ufpe.br>
2026-04-09 14:45:14 -0300

    feat(output): altera mensagem para versão A

--- projeto/main.py
+++ projeto/main.py
@@ -116,7 +116,7 @@ def calcular():
                 print("Opção inválida.")
                 continue
 
-            print(f"Resultado: {resultado}")
+            print(f"Resultado A: {resultado}")
 
         except ValueError:
             print("Entrada inválida.")
